bf8711a31da9b863c92c17842af177e6e8215e4c
geWhelan992 <gemma.m.whelan@gmail.com>
2025-12-28 14:15:46 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)  # Enables real-time communication
-
 
 @app.route("/")
 def home():
@@ -15,20 +12,11 @@ def index_page():
 
 @app.route("/intercom")
 def intercom_page():
-    return render_template("intercom.html")
-
-# ----- Simulated intercom trigger -----
-@app.route("/trigger_intercom/<intercom_name>/<message>")
-def trigger_intercom(intercom_name, message):
-    # Emit a real-time alert to all connected clients
-    socketio.emit('intercom_alert', {'intercom': intercom_name, 'message': message})
-    return f"Triggered intercom '{intercom_name}' with message: {message}"
-
+    return render_template("intercom.html")  # just your video page now
 
 @app.route("/instruction")
 def instruction_page():
     return render_template("instructionPage.html")
 
-
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    app.run(host='0.0.0.0', port=5000)
